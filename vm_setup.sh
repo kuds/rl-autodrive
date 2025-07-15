e3b0a24e5ebac90632eb8a43b30a440fdf335efc
kuds <michaelkudlaty@gmail.com>
2025-07-14 21:11:25 -0500

    Update vm_setup.sh

--- vm_setup.sh
+++ vm_setup.sh
@@ -6,10 +6,10 @@ sudo apt update && sudo NEEDRESTART_MODE=a apt upgrade -y
 sudo apt install -y ubuntu-desktop
 sudo ubuntu-drivers install
 sudo apt install -y python3-pip
-wget /Documents/ https://github.com/AutoDRIVE-Ecosystem/AutoDRIVE-RoboRacer-Sim-Racing/releases/download/2025-icra/autodrive_simulator_explore_linux.zip
-unzip autodrive_simulator_explore_linux.zip
-wget /Documents/ https://github.com/AutoDRIVE-Ecosystem/AutoDRIVE-RoboRacer-Sim-Racing/releases/download/2025-icra/autodrive_devkit.zip
-unzip autodrive_devkit.zip
+wget -P ./Documents/ https://github.com/AutoDRIVE-Ecosystem/AutoDRIVE-RoboRacer-Sim-Racing/releases/download/2025-icra/autodrive_simulator_explore_linux.zip
+unzip ./Documents/autodrive_simulator_explore_linux.zip
+wget -P ./Documents/ https://github.com/AutoDRIVE-Ecosystem/AutoDRIVE-RoboRacer-Sim-Racing/releases/download/2025-icra/autodrive_devkit.zip
+unzip ./Documents/autodrive_devkit.zip
 sudo reboot
 
 # Need to install ros2
