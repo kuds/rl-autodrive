5e9942f1b91e4dbde0bce7d4c09a204bf12e5ccd
kuds <michaelkudlaty@gmail.com>
2025-07-15 19:05:37 -0500

    Update vm_setup.sh

--- vm_setup.sh
+++ vm_setup.sh
@@ -24,16 +24,16 @@ wget -P ./Documents/ https://github.com/AutoDRIVE-Ecosystem/AutoDRIVE-RoboRacer-
 unzip ./Documents/autodrive_devkit.zip -d ./Documents/
 
 # Install ROS 2 Humble
-# sudo apt install -y software-properties-common
-# sudo add-apt-repository universe
-# sudo apt update && sudo apt install curl -y
-# export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
-# curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb" # If using Ubuntu derivates use $UBUNTU_CODENAME
-# sudo dpkg -i /tmp/ros2-apt-source.deb
-# sudo apt update
-# sudo NEEDRESTART_MODE=a apt upgrade -y
-# sudo apt install -y ros-humble-desktop
-# sudo apt install -y ros-dev-tools 
+sudo apt install -y software-properties-common
+sudo add-apt-repository universe
+sudo apt update && sudo apt install curl -y
+export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb" # If using Ubuntu derivates use $UBUNTU_CODENAME
+sudo dpkg -i /tmp/ros2-apt-source.deb
+sudo apt update
+sudo NEEDRESTART_MODE=a apt upgrade -y
+sudo apt install -y ros-humble-desktop
+sudo apt install -y ros-dev-tools 
 
 sudo reboot
 
